6429e8da2554270abaf94c5919b0519032e6a6c5
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2020-12-02 13:22:21 -0300

    Updates

--- chapter-07/7_2/src/components/EmailInput.vue
+++ chapter-07/7_2/src/components/EmailInput.vue
@@ -22,8 +22,7 @@ export default {
   },
   methods: {
     validateEmail(email) {
-      // eslint-disable-next-line no-useless-escape
-      const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      const regex = /^(([^\s"(),.:;<>@[\\\]]+(\.[^\s"(),.:;<>@[\\\]]+)*)|(".+"))@((\[(?:\d{1,3}\.){3}\d{1,3}])|(([\dA-Za-z\-]+\.)+[A-Za-z]{2,}))$/;
       return regex.test(email);
     },
   },
